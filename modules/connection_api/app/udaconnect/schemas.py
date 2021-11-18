fcb637c8a9ca2be0de699076c52db0a9e5706b7c
mmansoornano <mmansoor.nano@gmail.com>
2021-11-19 01:41:28 +0500

    udaconnect

---
+++ modules/connection_api/app/udaconnect/schemas.py
@@ -0,0 +1,10 @@
+from app.udaconnect.models import Connection
+from geoalchemy2.types import Geometry as GeometryType
+from marshmallow import Schema, fields
+from marshmallow_sqlalchemy.convert import ModelConverter as BaseModelConverter
+
+
+
+class ConnectionSchema(Schema):
+    location = fields.Nested(LocationSchema)
+    person = fields.Nested(PersonSchema)
